e28847ead2dcee149c464d3a50b497708ca89412
MichaelSingh7 <mikael.1@live.co.uk>
2019-10-28 17:18:09 +0000

    Creating An Option To Allow User To Find A Record Within My Menu

--- mongo_project.py
+++ mongo_project.py
@@ -26,18 +26,36 @@ def show_menu():
     return option
 
 
+def get_record():
+    print("")
+    first = input("Enter First Name? ")
+    last = input("Enter Last Name? ")
+
+    try:
+        doc = coll.find_one({'first': first.lower(), 'last': last.lower()})
+    except:
+        print("Access Denied Due To Processing Error")
+    if not doc:
+        print("")
+        print("Access Denied, No Results Found")
+
+    return doc
+
+
 def add_record():
     print("")
-    name = input("What Is Your Full Name?")
-    dob = input("Enter Your DOB --/--/----?")
-    gender = input("Please Enter Your Gender?")
-    hair_colour = input("Enter Your Natural Hair Colour?")
-    occupation = input("What Is Your Current Occupation?")
-    nationality = input("What Is Your Nationality?")
-    food = input("What Is Your Favourite Food?")
-    drink = input("What Is Your Favourite Drink?")
+    first = input("What Is Your First Name? ")
+    last = input("What Is Your Last Name? ")
+    dob = input("Enter Your DOB --/--/----? ")
+    gender = input("Please Enter Your Gender? ")
+    hair_colour = input("Enter Your Natural Hair Colour? ")
+    occupation = input("What Is Your Current Occupation? ")
+    nationality = input("What Is Your Nationality? ")
+    food = input("What Is Your Favourite Food? ")
+    drink = input("What Is Your Favourite Drink? ")
 
-    new_doc = {'name': name.lower(), 'dob': dob, 'gender': gender,
+    new_doc = {'first': first.lower(), 'last': last.lower(),
+               'dob': dob, 'gender': gender,
                'hair_colour': hair_colour, 'occupation': occupation,
                'nationality': nationality, 'food': food, 'drink': drink}
 
@@ -49,13 +67,22 @@ def add_record():
         print("Access Denied Due To Processing Error")
 
 
+def find_record():
+    doc = get_record()
+    if doc:
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + ": " + v.capitalize())
+
+
 def main_loop():
     while True:
         option = show_menu()
         if option == "1":
             add_record()
         elif option == "2":
-            print("Option 2 Selected")
+            find_record()
         elif option == "3":
             print("Option 3 Selected")
         elif option == "4":
